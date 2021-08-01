59ee52a5c9289f4813dd24fbd9d0dac579175431
Abigail Mathews <abbymathews@gmail.com>
2021-08-01 19:47:32 -0400

    Add auth using api key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional, List
-from fastapi import FastAPI, Depends, HTTPException, Body
+from fastapi import FastAPI, Depends, HTTPException, Body, Security
+from fastapi.security.api_key import APIKeyHeader
 from sqlalchemy.orm import Session
 
 import openai
@@ -12,13 +13,23 @@ import models
 import schemas
 from database import SessionLocal, engine
 
-from secrets import OPENAI_API_KEY
+from secrets import API_KEY, API_KEY_NAME, OPENAI_API_KEY
 
 models.Base.metadata.create_all(bind=engine)
 
 openai.api_key = OPENAI_API_KEY
 
-app = FastAPI()
+api_key_header_auth = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
+
+
+async def get_api_key(api_key_header: str = Security(api_key_header_auth)):
+    if api_key_header != API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid API Key'
+        )
+
+app = FastAPI(dependencies=[Security(get_api_key)])
 
 
 def get_db():
